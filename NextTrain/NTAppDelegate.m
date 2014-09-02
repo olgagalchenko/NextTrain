f57c4b8cb1004c4a1df41386fc05426854f232e5
Olga Galchenko <olga.v.galchenko@gmail.com>
2014-09-01 18:59:00 -0700

    Added status bar icon

--- NextTrain/NTAppDelegate.m
+++ NextTrain/NTAppDelegate.m
@@ -8,11 +8,36 @@
 
 #import "NTAppDelegate.h"
 
+@interface NTAppDelegate ()
+{
+    NSStatusItem* statusItem;
+    NSMenu* nextTrainMenu;
+}
+@end
+
 @implementation NTAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    [self activateStatusMenu];
 }
+
+- (void)activateStatusMenu
+{
+    NSStatusBar *bar = [NSStatusBar systemStatusBar];
+    
+    statusItem = [bar statusItemWithLength:NSVariableStatusItemLength];
+    
+    [statusItem setImage:[NSImage imageNamed:@"train"]];
+    [statusItem setHighlightMode:YES];
+    
+    nextTrainMenu = [[NSMenu alloc] init];
+    [nextTrainMenu addItemWithTitle:@"Next train is: " action:nil keyEquivalent:@""];
+    
+    [statusItem setMenu:nextTrainMenu];
+}
+
+
 
 @end
